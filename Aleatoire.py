486d16cd95e9d112f2a4bfdbd20ffe9030c557df
Punky37 <louvelouis@gmail.com>
2025-05-02 10:40:12 +0200

    Add files via upload

---
+++ Aleatoire.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 30 10:33:08 2025
+
+@author: loulou
+"""
+
+from random import *
+import time
+from compte import compte_points
+
+def construit(rinit, sinit):
+    rfin=[]
+    sfin=[]
+    
+    #choisit aléatoirement les 8 cartes régions à jouer
+    for n in range(8):
+        rfin.append(choices(rinit,k=1)[0])
+        rinit.remove(rfin[n])
+    compteur=0
+    
+    #compte le nombre de sanctuaires piochés
+    for k in range(7):
+        if rfin[k+1]>rfin[k]:
+            compteur+=1
+    
+    #choisit les sanctuaires aléatoirement
+    for n in range(compteur):
+        sfin.append(choices(sinit,k=1)[0])
+        sinit.remove(sfin[n])
+    return rfin+sfin
+    
+
+#separe les cartes regions et les cartes sanctuaire en 2 listes
+def separe(l):
+    condition=True
+    debut=[]
+    fin=[]
+    compteur=0
+    while condition and compteur<len(l):
+        if l[compteur]<100 :
+            debut.append(l[compteur])
+            compteur+=1
+        else :
+            fin+=l[compteur:]
+            condition=False
+    return (debut,fin)
+        
+
+def cherche_meilleur(instance : list):
+    meilleur_combinaison=[]
+    meilleur_score=0
+    
+    t=separe(instance)
+    region=t[0]
+    sanct=t[1]
+    
+    n=time.time()
+    while time.time()-n<5 :
+        combinaison=construit(region,sanct)
+        score=compte_points(combinaison)
+        if score>meilleur_score:
+            meilleur_combinaison=combinaison.copy()
+            meilleur_score=score
+    return (meilleur_combinaison, meilleur_score)
