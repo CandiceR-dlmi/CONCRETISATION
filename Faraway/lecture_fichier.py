35929408e0bf68e4b71ebf2f7ee111ea5515d4aa
CandiceR-dlmi <candiceragey@gmail.com>
2025-05-22 15:27:50 +0200

    Update lecture_fichier.py

--- Faraway/lecture_fichier.py
+++ Faraway/lecture_fichier.py
@@ -19,7 +19,7 @@ def lecture(nomf : str) -> list:
             l[k]=int(l[k])
         return l
 
-def sortie(tab : tuple, i=1) -> None:
+def sortie(tab : tuple, instance : str) -> None:
     """
     Ecrit le résultat dans un fichier
     
@@ -27,8 +27,8 @@ def sortie(tab : tuple, i=1) -> None:
     ----------
     tab : tuple
         tuple : (liste des cartes, score)
-    i : int, optional
-        Numéro du fichier, 1 par défaut
+    instance : str
+        Nom du fichier
 
     Returns
     -------
@@ -36,7 +36,7 @@ def sortie(tab : tuple, i=1) -> None:
         Un fichier nommé 'resultat_competition_{numéro du fichier}' est créé
 
     """
-    with open(f"resultat_competition_{i}.txt", "w") as fichier:
+    with open("resultat_"+instance, "w") as fichier:
         for k in range(len(tab[1])):
             fichier.write(str(tab[1][k]))
             fichier.write(" ")
