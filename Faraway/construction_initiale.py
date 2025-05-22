7b5d5cf04835af0d4c7893841280a8b15acddccc
CandiceR-dlmi <candiceragey@gmail.com>
2025-05-22 15:26:59 +0200

    Update construction_initiale.py

--- Faraway/construction_initiale.py
+++ Faraway/construction_initiale.py
@@ -25,7 +25,7 @@ def les_meilleures(instance:list) -> list:
     sanct=t[1]
     
     n=time.time()
-    while time.time()-n<10 :
+    while time.time()-n<len(instance)/80 :
         combinaison=construit(region.copy(),sanct.copy())
         score=compte_points(combinaison)
         if score>meilleur_score:
